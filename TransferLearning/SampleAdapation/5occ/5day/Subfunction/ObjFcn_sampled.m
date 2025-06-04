9eb6a835193c3af22149ab6c8e50c635bfc7f7ca
Yicheng Li <74645033+Lee-18@users.noreply.github.com>
2025-06-04 00:45:47 -0400

    Delete TransferLearning/SampleAdapation/5occ directory

--- TransferLearning/SampleAdapation/5occ/5day/Subfunction/ObjFcn_sampled.m
+++
@@ -1,5 +0,0 @@
-function mmdValue = ObjFcn_sampled(sampleIndices, samplesA, samplesB, sigma)
-    sampledC = samplesA(sampleIndices,:);
-    % 计算样本集C和B之间的MMD
-    mmdValue = calculateMMD(samplesB, sampledC, sigma);
-end
