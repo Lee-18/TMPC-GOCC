9750a3407b32266a207dac563779868f86ecf1a6
Yicheng Li <74645033+Lee-18@users.noreply.github.com>
2025-06-04 00:36:46 -0400

    Add files via upload

---
+++ TransferLearning/SampleAdapation/Group1/TL_eff/run.bat
@@ -0,0 +1,68 @@
+@ECHO OFF
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\5occ\1day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\5occ\2day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\5occ\3day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\5occ\4day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\5occ\5day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\10occ\1day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\10occ\2day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\10occ\3day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\10occ\4day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\10occ\5day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+
+
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\15occ\1day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\15occ\2day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\15occ\3day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\15occ\4day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\15occ\5day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+
+
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\7occ\1day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\7occ\2day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\7occ\3day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\7occ\4day
+matlab -batch "TransferLearing_DomainAdatation; exit"
+
+cd C:\FinalThesis\Project\TL_20240823\TL_eff_more\7occ\5day
+matlab -batch "TransferLearing_DomainAdatation; exit"
