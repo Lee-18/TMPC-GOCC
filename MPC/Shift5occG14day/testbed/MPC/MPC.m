f16ffaed30f6e8a1b41ddbb24bf4f07bcb283d97
Yicheng Li <74645033+Lee-18@users.noreply.github.com>
2025-06-11 10:51:06 -0400

    Add files via upload

---
+++ MPC/Shift5occG14day/testbed/MPC/MPC.m
@@ -0,0 +1,220 @@
+function MPC(block)
+%MSFUNTMPL_BASIC A Template for a Level-2 MATLAB S-Function
+%   The MATLAB S-function is written as a MATLAB function with the
+%   same name as the S-function. Replace 'msfuntmpl_basic' with the 
+%   name of your S-function.
+
+%   Copyright 2003-2018 The MathWorks, Inc.
+
+%%
+%% The setup method is used to set up the basic attributes of the
+%% S-function such as ports, parameters, etc. Do not add any other
+%% calls to the main body of the function.
+%%
+setup(block);
+
+%endfunction
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the basic characteristics of the S-function block such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C MEX counterpart: mdlInitializeSizes
+%%
+function setup(block)
+
+% Register number of ports
+block.NumInputPorts  = 1;
+block.NumOutputPorts = 1;
+
+% Setup port properties to be inherited or dynamic
+block.SetPreCompInpPortInfoToDynamic;
+block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+block.InputPort(1).Dimensions        = 6;
+block.InputPort(1).DatatypeID  = 0;  % double
+block.InputPort(1).Complexity  = 'Real';
+block.InputPort(1).DirectFeedthrough = true;
+
+% Override output port properties
+block.OutputPort(1).Dimensions       = 2;
+block.OutputPort(1).DatatypeID  = 0; % double
+block.OutputPort(1).Complexity  = 'Real';
+
+
+% Register parameters
+block.NumDialogPrms     = 0;
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+%% -----------------------------------------------------------------
+%% The MATLAB S-function uses an internal registry for all
+%% block methods. You should register all relevant methods
+%% (optional and required) as illustrated below. You may choose
+%% any suitable name for the methods and implement these methods
+%% as local functions within the same file. See comments
+%% provided for each function for more information.
+%% -----------------------------------------------------------------
+
+% block.RegBlockMethod('PostPropagationSetup',    @DoPostPropSetup);
+% block.RegBlockMethod('InitializeConditions', @InitializeConditions);
+% block.RegBlockMethod('Start', @Start);
+block.RegBlockMethod('Outputs', @Outputs);     % Required
+% block.RegBlockMethod('Update', @Update);
+% block.RegBlockMethod('Derivatives', @Derivatives);
+block.RegBlockMethod('Terminate', @Terminate); % Required
+
+%end setup
+
+%%
+%% PostPropagationSetup:
+%%   Functionality    : Setup work areas and state variables. Can
+%%                      also register run-time methods here
+%%   Required         : No
+%%   C MEX counterpart: mdlSetWorkWidths
+%%
+function DoPostPropSetup(block)
+% block.NumDworks = 1;
+%   
+%   block.Dwork(1).Name            = 'x1';
+%   block.Dwork(1).Dimensions      = 1;
+%   block.Dwork(1).DatatypeID      = 0;      % double
+%   block.Dwork(1).Complexity      = 'Real'; % real
+%   block.Dwork(1).UsedAsDiscState = true;
+
+
+%%
+%% InitializeConditions:
+%%   Functionality    : Called at the start of simulation and if it is 
+%%                      present in an enabled subsystem configured to reset 
+%%                      states, it will be called when the enabled subsystem
+%%                      restarts execution to reset the states.
+%%   Required         : No
+%%   C MEX counterpart: mdlInitializeConditions
+%%
+function InitializeConditions(block)
+
+%end InitializeConditions
+
+
+%%
+%% Start:
+%%   Functionality    : Called once at start of model execution. If you
+%%                      have states that should be initialized once, this 
+%%                      is the place to do it.
+%%   Required         : No
+%%   C MEX counterpart: mdlStart
+%%
+function Start(block)
+block.Dwork(1).Data = 0;
+
+%end Start
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   Required         : Yes
+%%   C MEX counterpart: mdlOutputs
+%%
+function Outputs(block)
+persistent Speed_opt settings NumOcc ph Tlb Tub GEB GroupOcc TL
+if isempty(settings)
+    settings=readtable('settings.csv');
+    NumOcc=settings.NumOcc(1);
+    ph=settings.ph(1);
+    Tlb=settings.Tlb(1);
+    Tub=settings.Tub(1);
+    GEB=settings.GEB(1);
+    GroupOcc=settings.GroupOcc(1);
+    TL=settings.TL(1);
+end
+input=block.InputPort(1).Data;
+time_sim=input(1);
+Tz=input(2);
+RHz=input(3);
+To=input(4);
+RHo=input(5);
+m_sup=input(6);
+
+if mod(time_sim,86400)<5*3600 || mod(time_sim,86400)>=20*3600
+    cooling_stp=(90-32)/1.8;
+else
+    if mod(mod(time_sim,86400),15*60)==0
+        Speed_opt=Speed_determine(time_sim,Tz,RHz,To,RHo,NumOcc,ph,Tlb,Tub,GEB,GroupOcc,TL);
+    end
+    Speed=Speed_opt;
+    if Speed==0
+        cooling_stp = Tub+10;
+    elseif Speed==1
+        if m_sup<0.3  % determine system current mode based on m_sup
+            cooling_stp=Tz-0.75/1.8;
+        elseif m_sup>=0.3 && m_sup<0.5
+            cooling_stp=Tz-0.5/1.8;
+        elseif m_sup>=0.5
+            cooling_stp=Tz-0.25/1.8;
+        end
+    elseif Speed==2
+        cooling_stp = Tlb-5;
+    end
+end
+
+heating_stp=10;
+
+block.OutputPort(1).Data = [cooling_stp,heating_stp];
+
+%end Outputs
+
+%%
+%% Update:
+%%   Functionality    : Called to update discrete states
+%%                      during simulation step
+%%   Required         : No
+%%   C MEX counterpart: mdlUpdate
+%%
+function Update(block)
+
+% block.Dwork(1).Data = block.InputPort(1).Data;
+
+%end Update
+
+%%
+%% Derivatives:
+%%   Functionality    : Called to update derivatives of
+%%                      continuous states during simulation step
+%%   Required         : No
+%%   C MEX counterpart: mdlDerivatives
+%%
+function Derivatives(block)
+
+%end Derivatives
+
+%%
+%% Terminate:
+%%   Functionality    : Called at the end of simulation for cleanup
+%%   Required         : Yes
+%%   C MEX counterpart: mdlTerminate
+%%
+function Terminate(block)
+
+%end Terminate
